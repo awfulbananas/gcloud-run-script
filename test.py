3384e3b85779b98f393a364426f9b2237a9ff5ba
awfulbananas <Joseph.Fromel@gmail.com>
2024-04-09 10:27:43 -0700

    fixed a syntax error in test.py

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from download_yt_audio import get_videos
 def main():
 	get_videos("/usr/src/app/data", "https://www.youtube.com/user/wanderbots/")
 
-if(__name__=="__main__")
+if(__name__=="__main__"):
 	main()
