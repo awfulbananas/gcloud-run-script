9f56c26f18fd8461b0d9fe9b8c4991cae804b8a7
awfulbananas <Joseph.Fromel@gmail.com>
2024-04-11 19:06:15 -0700

    sorting vastai offers that meet the specs by cost

--- vast_manager.py
+++ vast_manager.py
@@ -2,8 +2,20 @@ import subprocess
 from subprocess import PIPE
 
 searchCall = ['vastai', 'search offers', 'cpu_cores>=10 gpu_total_ram>=8 reliability>=0.95 dph<2']
-def find_instance():
+
+def find_instance() -> str:
     searchOut = subprocess.run(searchCall, check=True, stdout=PIPE, text=True).stdout
     rows = searchOut.split('\n')
+    if(len(rows) < 2):
+        return "Failed"
     table = [row.split() for row in rows]
-    print(table)
+    tableHeader = table[0]
+    tableData = table[range(1, len(table))]
+    costInd = tableHeader.index("$/hr")
+    def data_sorter(row):
+        return row[costInd]
+    
+    tableData.sort(key=data_sorter)
+    print(tableHeader)
+    print(tableData)
+    return ""
