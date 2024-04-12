3613ff9b4b841ae6ac5ab83591a99215649113d1
awfulbananas <Joseph.Fromel@gmail.com>
2024-04-11 18:53:43 -0700

    made it output text rather than bytes

--- vast_manager.py
+++ vast_manager.py
@@ -3,7 +3,7 @@ from subprocess import PIPE
 
 searchCall = ['vastai', 'search offers', 'cpu_cores>=10 gpu_total_ram>=8 reliability>=0.95 dph<2']
 def find_instance():
-    searchOut = subprocess.run(searchCall, check=True, stdout=PIPE).stdout
+    searchOut = subprocess.run(searchCall, check=True, stdout=PIPE, text=True).stdout
     rows = searchOut.split('\n')
     table = [row.split() for row in rows]
     print(table)
