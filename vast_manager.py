cdb4a2e8c21037d74a842f47f90ab17cbd8a9121
awfulbananas <Joseph.Fromel@gmail.com>
2024-04-11 18:41:01 -0700

    fixed vastai search args

--- vast_manager.py
+++ vast_manager.py
@@ -1,7 +1,7 @@
 import subprocess
 from subprocess import PIPE
 
-searchCall = ['vastai', 'search offers', 'cpu_cores>=10 total_gpu_ram>=8 reliability>=0.95 dph<2']
+searchCall = ['vastai', 'search offers', 'cpu_cores>=10 gpu_total_ram>=8 reliability>=0.95 dph<2']
 def find_instance():
     searchOut = subprocess.run(searchCall, check=True, stdout=PIPE).stdout
     print(searchOut)
