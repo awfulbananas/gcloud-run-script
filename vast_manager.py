c6b9a3d5c7dfe2b6f0ce01c3774ddc958f26f64b
awfulbananas <Joseph.Fromel@gmail.com>
2024-04-11 19:19:27 -0700

    fixed bug with data sorting

--- vast_manager.py
+++ vast_manager.py
@@ -6,11 +6,12 @@ searchCall = ['vastai', 'search offers', 'cpu_cores>=10 gpu_total_ram>=8 reliabi
 def find_instance() -> str:
     searchOut = subprocess.run(searchCall, check=True, stdout=PIPE, text=True).stdout
     rows = searchOut.split('\n')
-    if(len(rows) < 2):
-        return "Failed"
     table = [row.split() for row in rows]
     tableHeader = table[0]
-    tableData = [table[i] for i in range(1, len(table))]
+    #minus one because there's a trailing value which messes with the sorting
+    tableData = [table[i] for i in range(1, len(table) - 1)]
+    if(len(tableData) < 1):
+        return "Failed"
     costInd = tableHeader.index("$/hr")
     print("     unsorted:")
     print(tableHeader)
